1c66fd530ff3a07f474f521cf9b6327ac113ae5a
xiaoshenge <xiaoshengeer@gmail.com>
2012-11-22 16:58:47 +0800

    replace images

--- pro84/application/views/zh/goods_info.php
+++ pro84/application/views/zh/goods_info.php
@@ -32,7 +32,7 @@
 <div id="qqfloat" style="width:90px; height:150px;z-index: 999; left: 90%; visibility: visible; position: absolute; top: 259px;">
 <div style="width:8px; height:150px; float:left; background:url(http://im.bizapp.qq.com:8000/bg_1.gif) no-repeat;"></div>
 <div style="float:left; width:74px; height:150px; background:url(http://im.bizapp.qq.com:8000/middle.jpg) no-repeat; ">
-        <div style="height:80px;  clear:both;margin-top:40px;width:74px; float:left; background:url(http://im.bizapp.qq.com:8000/face.jpg) no-repeat;"></div>
+        <div style="height:80px;  clear:both;margin-top:40px;width:74px; float:left; background:url(/images/face.jpg) no-repeat;"></div>
         <div style="clear:both;width:74px;">
         <a style="padding-top:15px;height:20px; background:url(http://im.bizapp.qq.com:8000/btn_2.gif) no-repeat;  display:block;" target="_blank" href="tencent://message/?uin=71811124"></a>
         </div>
